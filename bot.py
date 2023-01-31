30b8bceb0205f606e6bd2ed269c9676274498256
catoo-hub <cato.ixq@gmail.com>
2023-01-31 23:31:47 +0400

    g

---
+++ bot.py
@@ -0,0 +1,103 @@
+import nextcord, asyncio
+from nextcord.ext import commands
+
+from utils.config import *
+
+from datetime import datetime
+from pytz import timezone
+
+
+# Form Modal Class
+class FormModal(nextcord.ui.Modal):
+
+	# Form Modal Constructor
+	def __init__(self):
+		super().__init__(
+			title = title,
+			custom_id = app_custom_id,
+			timeout = None
+		)
+
+		# Form Modal Fields
+
+		for i in range(0, len(questions)):
+			if questions[i]["style"] == "input":
+				self.field = nextcord.ui.TextInput(
+					label = questions[i]["question"], 
+					placeholder = questions[i]["placeholder"], 
+					required = questions[i]["required"],
+					style = nextcord.TextInputStyle.short if questions[i]["style"] == "short" else nextcord.TextInputStyle.paragraph,
+					custom_id = questions[i]["custom_id"],			
+				)
+
+			self.add_item(self.field)
+
+	# Modal Callback
+	async def callback(self, interaction: nextcord.Interaction) -> None:
+		embed = nextcord.Embed(
+			color = nextcord.Color.from_rgb(163, 128, 255),
+			timestamp = datetime.now(timezone("US/Eastern")),
+		).set_author(
+			name = f"{interaction.user}'s Application",
+			icon_url = interaction.user.avatar.url
+		)
+
+		# Form Embed Fields
+		for question, answer in zip(questions, self.children):
+			embed.add_field(
+				name = question["question"],
+				value = answer.value if answer.value else question_not_answered_message,
+				inline = False
+			)
+
+		# Sending Form Embed
+		await interaction.response.send_message(submit_message, ephemeral = True)
+		submit_channel = await bot.fetch_channel(submit_channel_id)
+		await submit_channel.send("<@&1028779669950042122>, check this", embed=embed)
+
+# Form View Class
+class FormView(nextcord.ui.View):
+
+	# Form View Constructor
+	def __init__(self):
+		super().__init__(timeout = None)
+
+	# Form View Button Callback
+	@nextcord.ui.button(label = button_name, style = nextcord.ButtonStyle.green, emoji = "✅", custom_id = "forms:button")
+	async def button_callback(self, button : nextcord.ui.Button, interaction : nextcord.Interaction):
+		await interaction.response.send_modal(FormModal())
+
+# Bot Subclass
+class Bot(commands.Bot):
+
+	# Constructor
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.persistant_modals_added = False
+		self.persistant_views_added = False
+
+	# Ready Event
+	async def on_ready(self):
+		print(f"Bot is ready! | Logged in as {self.user} (ID: {self.user.id})")
+
+		if self.persistant_modals_added == False:
+			self.persistant_modals_added = True
+			self.add_modal(FormModal())
+		
+		if self.persistant_views_added == False:
+			self.persistant_views_added = True
+			self.add_view(FormView())
+
+		print(f"----------------------------------\n       LAUNCH INFORMATION\n----------------------------------\nStarted: True\nDebug Mode: False\nDate: {datetime.now(tz = timezone('US/Eastern')).strftime('%Y-%m-%d | Time: %H:%M:%S')}\nPersistant Views Added: {str(self.persistant_views_added)}\nPersistant Modals Added: {str(self.persistant_modals_added)}\n----------------------------------")			
+
+# Bot Instance
+bot = Bot(command_prefix = PREFIX, intents = nextcord.Intents.all(), help_command = None)
+
+# Load Modules
+for file in os.listdir("./modules"):
+	if file.endswith(".py"):
+		bot.load_extension(f"modules.{file[:-3]}")
+		print("Loaded module: " + file[:-3])
+
+# Run Bot
+bot.run(TOKEN)
---
+++ modules/bot_commands.py
@@ -0,0 +1,182 @@
+import nextcord, asyncio
+from nextcord.ext import commands
+from bot import FormView
+
+from utils.config import *
+
+from utils.questions_generator import Question
+
+# Confirmation Buttons View
+class ConfirmationView(nextcord.ui.View):
+
+	def __init__(self, user : nextcord.Member):
+		super().__init__(timeout = confirmation_timeout)
+		self.value = None
+		self.user = user
+
+	# Timeout Event
+	async def on_timeout(self):
+		for child in self.children:
+			child.disabled = True
+
+		await self.message.edit(content = configuration_timeout_confirmation_message, view = self)
+
+		self.stop()
+
+	# Confirm Button
+	@nextcord.ui.button(label = "I'm sure, exit.", style = nextcord.ButtonStyle.red)
+	async def confirm(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+		if self.user == interaction.user.id:
+			await interaction.response.send_message(configuration_cancel_message, ephemeral = True)
+			self.value = True
+			for child in self.children:
+				child.disabled = True
+			await interaction.message.edit(content = configuration_cancel_message, view = self)
+
+			self.stop()
+		else:
+			return await interaction.response.send_message("This is not for you!", ephemeral = True)
+
+	# Cancel Button
+	@nextcord.ui.button(label = "Never mind, let's keep it up.", style = nextcord.ButtonStyle.green)
+	async def cancel(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+		if self.user == interaction.user.id:
+			await interaction.response.send_message(configuration_not_cancelled_message, ephemeral = True)
+			self.value = False
+			for child in self.children:
+				child.disabled = True
+			await interaction.message.edit(content = configuration_not_cancelled_message, view = self)
+
+			self.stop()
+		else:
+			return await interaction.response.send_message("This is not for you!", ephemeral = True)
+
+# Configuration Cog	
+class Configuration(commands.Cog, name = "Bot Configuration Command"):
+	"""Bot commands module."""
+	
+	# Configuration Constructor
+	def __init__(self, bot : commands.Bot):
+		self.bot = bot
+		self.configurator = None
+
+	# Config Command
+	@commands.command(name = "config", description = "Configure the bot.")
+	@commands.guild_only()
+	@commands.has_permissions(administrator = True)
+	async def config_bot(self, ctx : commands.Context):
+		if self.configurator != None and self.configurator != ctx.author.id:
+			return await ctx.send(configuration_error_message)
+
+		self.configurator = ctx.author.id
+
+		question = Question(ctx, self.bot)
+		alive = await question.ask_config_question(start_config_message)
+
+		if alive == True:
+			alive = await question.ask_config_question(app_title_message)
+
+		if alive == True:
+			alive = await question.ask_config_question(app_custom_id_message)
+
+		if alive == True:
+			alive = await question.ask_config_question(app_button_name_message)
+
+		if alive == True:
+			alive = await question.ask_config_question(app_embed_name_message)
+
+		if alive == True:
+			alive = await question.ask_config_question(app_embed_description_message)
+
+		if alive == True:	
+			alive = False	
+
+			return await ctx.send(config_end.format(PREFIX, PREFIX))
+
+	# Add Questions Command
+	@commands.command(name = "addquestion", description = "Adds questions to the application form.")
+	@commands.has_permissions(administrator = True)
+	async def add_question(self, ctx : commands.Context):
+		if self.configurator != None and self.configurator != ctx.author.id:
+			return await ctx.send(configuration_error_message)
+		
+		self.configurator = ctx.author.id
+
+		question = Question(ctx, self.bot)
+		alive = await question.ask_questions(question_message)
+
+		if alive == True:
+			alive = await question.ask_questions(placeholder_message)
+
+		if alive == True:
+			alive = await question.ask_questions(required_message)
+
+		if alive == True:
+			alive = await question.ask_questions(style_message)
+
+		if alive == True:
+			alive = await question.ask_questions(input_style_message)
+
+		if alive == True:
+			alive = await question.ask_questions(question_custom_id_message)
+
+		if alive == True:	
+			alive = False	
+
+			return await ctx.send(questions_end)
+
+	# Send Form Command
+	@commands.command(name = "sendform", description = send_button_to_channel_description)
+	@commands.has_permissions(administrator = True)
+	async def send_modal(self, ctx : commands.Context, channel : nextcord.TextChannel = None):
+		if not channel:
+			channel = ctx.channel
+
+		embed = nextcord.Embed(
+			title = embed_name,
+			description = embed_description,
+			color = nextcord.Color.from_rgb(163, 128, 255)
+		).set_image(url="https://media.discordapp.net/attachments/919251928754585651/1065231467099213854/232.png?width=1440&height=1")
+
+		await ctx.reply(sent_button_to_channel_message)
+		await channel.send(embed = embed, view = FormView())
+
+	#hui
+	@commands.command(name = "sendimage", description = "for apps")
+	@commands.has_permissions(administrator = True)
+	async def config_bot(self, ctx : commands.Context, channel : nextcord.TextChannel = None):
+		embedimg = nextcord.Embed(
+			color = nextcord.Color.from_rgb(163, 128, 255)
+		).set_image(url="https://media.discordapp.net/attachments/919251928754585651/1065231467099213854/232.png?width=1440&height=420")
+
+		await channel.send(embed = embedimg)
+	
+	# Force Leave Command
+	@commands.command(name = "forceleave", description = "Force Leave Configuration Mode.")
+	@commands.has_permissions(administrator = True)
+	async def force_leave(self, ctx : commands.Context):
+		if self.configurator == ctx.author.id:
+			self.configurator = None
+
+			global questions_alive
+			questions_alive = False
+
+			await ctx.send("Forcing configuration mode to end...")
+		else:
+			await ctx.send("Only the person who started the configuration mode can force it to end.")
+			return
+
+	# Command To Add Commands
+	@commands.command(name = "addcommand", description = "Add a command to the bot.")
+	@commands.has_permissions(administrator = True)
+	async def add_command(self, ctx : commands.Context, command : str):
+		if command in self.bot.commands:
+			await ctx.send("That command already exists.")
+			return
+
+		self.bot.add_command(command)
+		await ctx.send("Command added.")
+
+# Setup Function
+def setup(bot : commands.Bot):
+	bot.add_cog(Configuration(bot))
